a506b4b80365e144f5aa1409dd92c22c7bdada4e
Soon Van <cog@randomecho.com>
2017-02-12 23:24:52 -0500

    Use viper to parse configuration stored in YAML

--- sugar-gobot.go
+++ sugar-gobot.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 func main() {
+	viper.SetConfigName("app")
+	viper.AddConfigPath("config")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		fmt.Println("Could not read config/app file")
+	} else {
+		site_url := viper.GetString("site_url")
+
+		fmt.Println("Connecting with", site_url)
+	}
 }
