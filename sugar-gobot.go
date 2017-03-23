cb286f02476332d6237de9dd9a06e4c59339d764
Soon Van <cog@randomecho.com>
2017-03-23 16:15:03 -0400

    Connect to Sugar API with credentials
    Only as far as being able to lob something into the Sugar
    instance and getting a response back. Need to actually
    parse the response to make sense of it.

--- sugar-gobot.go
+++ sugar-gobot.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/viper"
+	"net/http"
 )
 
 func main() {
@@ -15,7 +18,26 @@ func main() {
 		fmt.Println("Could not read config/app file")
 	} else {
 		site_url := viper.GetString("site_url")
+		username := viper.GetString("username")
+		password := viper.GetString("password")
 
-		fmt.Println("Connecting with", site_url)
+		fmt.Println("Connecting with", username, "to", site_url)
+
+		creds := map[string]string{
+			"username":      username,
+			"password":      password,
+			"grant_type":    "password",
+			"client_id":     "sugar",
+			"client_secret": "",
+			"platform":      "api",
+		}
+		byteCreds, _ := json.Marshal(creds)
+		response, err := http.Post(site_url+"oauth2/token", "application/json", bytes.NewBuffer(byteCreds))
+
+		if err != nil {
+			fmt.Println("Error", err)
+		} else {
+			fmt.Println("Response", response)
+		}
 	}
 }
