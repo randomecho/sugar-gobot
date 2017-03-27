1f5f562a6e7ea03817c1b83f93b7ae7d5c819f9b
Soon Van <cog@randomecho.com>
2017-03-26 21:43:11 -0400

    Use more log.Fatal to reduce all the indents
    Instead of all the if/else controls, the use of just
    log.Fatal to spit out the error and moving on cuts down
    on how far more indentation needs to happen.
    
    Adds use of defer to close out the HTTP request from
    the fetch. Not sure it needs to happen, but reading
    so far suggests it is a clean thing to do.

--- sugar-gobot.go
+++ sugar-gobot.go
@@ -6,71 +6,77 @@ import (
 	"fmt"
 	"github.com/spf13/viper"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
 	viper.SetConfigName("app")
 	viper.AddConfigPath("config")
-
 	err := viper.ReadInConfig()
 
 	if err != nil {
-		fmt.Println("Could not read config/app file")
-	} else {
-		site_url := viper.GetString("site_url")
-		username := viper.GetString("username")
-		password := viper.GetString("password")
-		var jsonBody map[string]interface{}
-
-		fmt.Println("Connecting with", username, "to", site_url)
-
-		creds := map[string]string{
-			"username":      username,
-			"password":      password,
-			"grant_type":    "password",
-			"client_id":     "sugar",
-			"client_secret": "",
-			"platform":      "api",
-		}
-		byteCreds, _ := json.Marshal(creds)
-		response, err := http.Post(site_url+"oauth2/token", "application/json", bytes.NewBuffer(byteCreds))
-
-		if err != nil {
-			fmt.Println("Error", err)
-		} else {
-			if response.StatusCode == 200 {
-				bodyInBytes, _ := ioutil.ReadAll(response.Body)
-				json.Unmarshal(bodyInBytes, &jsonBody)
-				accessToken := jsonBody["access_token"]
-
-				fmt.Println("access_token received", accessToken)
-
-				record := map[string]string{
-					"name": "Gallsaberry",
-				}
-
-				recordJSON, _ := json.Marshal(record)
-				request, _ := http.NewRequest("POST", site_url+"Accounts", bytes.NewBuffer(recordJSON))
-				request.Header.Set("Content-Type", "application/json")
-				request.Header.Add("oauth-token", accessToken.(string))
-
-				client := &http.Client{}
-				response, err := client.Do(request)
-
-				if err != nil {
-					fmt.Println("Error on create", err)
-				} else {
-					if response.StatusCode == 200 {
-						bodyInBytes, _ := ioutil.ReadAll(response.Body)
-						json.Unmarshal(bodyInBytes, &jsonBody)
-
-						fmt.Println("Created Account with ID", jsonBody["id"])
-					}
-
-				}
-
-			}
-		}
+		log.Fatal("Could not read config/app file")
 	}
+
+	site_url := viper.GetString("site_url")
+	username := viper.GetString("username")
+	password := viper.GetString("password")
+	var jsonBody map[string]interface{}
+
+	fmt.Println("Connecting with", username, "to", site_url)
+
+	creds := map[string]string{
+		"username":      username,
+		"password":      password,
+		"grant_type":    "password",
+		"client_id":     "sugar",
+		"client_secret": "",
+		"platform":      "api",
+	}
+	byteCreds, _ := json.Marshal(creds)
+	responseOAuth, err := http.Post(site_url+"oauth2/token", "application/json", bytes.NewBuffer(byteCreds))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer responseOAuth.Body.Close()
+
+	if responseOAuth.StatusCode != 200 {
+		log.Fatal(responseOAuth)
+	}
+
+	bodyInBytes, _ := ioutil.ReadAll(responseOAuth.Body)
+	json.Unmarshal(bodyInBytes, &jsonBody)
+	accessToken := jsonBody["access_token"]
+
+	fmt.Println("access_token received", accessToken)
+
+	record := map[string]string{
+		"name": "Gallsaberry",
+	}
+
+	recordJSON, _ := json.Marshal(record)
+	request, _ := http.NewRequest("POST", site_url+"Accounts", bytes.NewBuffer(recordJSON))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Add("oauth-token", accessToken.(string))
+
+	client := &http.Client{}
+	responseCreate, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(responseOAuth)
+	}
+
+	defer responseCreate.Body.Close()
+
+	if responseCreate.StatusCode != 200 {
+		log.Fatal(responseCreate)
+	}
+
+	responseCreateInBytes, _ := ioutil.ReadAll(responseCreate.Body)
+	json.Unmarshal(responseCreateInBytes, &jsonBody)
+
+	fmt.Println("Created Account with ID", jsonBody["id"])
 }
